def6bd8491e4744c443be6ffed81048e97fb58ed
Ose Ogbe <therealnylat@gmail.com>
2022-10-06 07:50:07 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <Hero />
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'HomePage',
+
+    data() {
+      return {
+        movies: [],
+      }
+    },
+
+    methods: {
+      async getMovies() {
+        const data = axios.get(
+          `https://api.themoviedb.org/3/movie/now_playing?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1`
+        )
+        const result = await data
+        result.data.results.forEach((movie) => {
+          this.movies.push(movie)
+        })
+      },
+    },
+  }
+  </script>
+
+  <style lang="scss">
+
+  </style>
